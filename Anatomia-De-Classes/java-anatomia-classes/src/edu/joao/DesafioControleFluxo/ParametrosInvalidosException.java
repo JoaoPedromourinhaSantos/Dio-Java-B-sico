a716ed368d9bdfd4332d90463d60056a6578dfc9
Moura <joaopedros639@gmail.com>
2024-06-02 16:22:01 -0300

    Mudanças e desafios

---
+++ Anatomia-De-Classes/java-anatomia-classes/src/edu/joao/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package edu.joao.DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
--- Anatomia-De-Classes/java-anatomia-classes/src/edu/joao/Escopo/Escopo.java
+++
@@ -1,5 +0,0 @@
-package edu.joao.Escopo;
-
-public class Escopo {
-    
-}
